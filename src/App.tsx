5df14c1a6b83393dfc4f0445180104a24b2b9ba2
Chandra-Moulii <chandramouliv22@gmail.com>
2024-08-03 13:33:22 +0530

    feat: project init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="my-5 space-y-1">
+      <center className="text-lg font-medium">Hola, Mouli 👋 </center>
+      <center>Good luck with your new project 💞</center>
+    </div>
+  );
+}
+
+export default App;
